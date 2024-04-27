500d439308e5d75ffc4412dbde35e3976a4aac91
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-27 09:37:16 +0530

    make mongoDB database connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
 
+// connection to DB 
+const { connectDB } = require('./config/database');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server Started on PORT ${PORT}`);
+    console.log(`Server Started on PORT ${PORT} 🤩`);
 });
 
 
-
+// connections
+connectDB();
 
 
 
