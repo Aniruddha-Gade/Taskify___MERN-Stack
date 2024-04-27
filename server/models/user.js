d82d98986e5275032d27820a3b247805b7aa8202
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-27 10:41:33 +0530

    add: user & profile schemas

---
+++ server/models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        // accountType: {
+        //     type: String,
+        //     enum: ['Admin', 'client'],
+        //     reuired: true
+        // },
+        additionalDetails: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Profile",
+            required: true,
+        },
+        tasks: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Task",
+            },
+        ],
+        image: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+        },
+        resetPasswordTokenExpires: {
+            type: Date,
+        },
+    }, // Add timestamps for when the document is created and last modified
+    { timestamps: true }
+);
+
+const User = mongoose.models?.User || mongoose.model("User", UserSchema);
+
+export default User;
