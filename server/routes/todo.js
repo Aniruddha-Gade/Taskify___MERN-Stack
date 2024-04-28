d1a8b624f84bab50e13044b9960e7ef0aa7dd9b1
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-28 14:22:02 +0530

    add: authentication for all routes

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTodo, getAllTodos, updateTodo, deleteTodo } = require('../controllers/todo')
+const { createTodo, getAllTodos, updateTodo, deleteTodo } = require('../controllers/todo');
+const { auth } = require('../middleware/auth');
 
 // create todo route
-router.post('/create-todo', createTodo)
-router.post('/all-todos', getAllTodos)
-router.put('/update-todo', updateTodo)
-router.delete('/delete-todo', deleteTodo)
+router.post('/create-todo', auth, createTodo)
+router.post('/all-todos', auth, getAllTodos)
+router.put('/update-todo', auth, updateTodo)
+router.delete('/delete-todo', auth, deleteTodo)
 
 
 module.exports = router
