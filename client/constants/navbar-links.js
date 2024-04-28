b0510b7bf2755fe92e4039a7be0b2b5e63018261
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-28 23:51:20 +0530

    add: pages for home, dashboard, contact, about

---
+++ client/constants/navbar-links.js
@@ -0,0 +1,18 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+];
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes, } from "react-router-dom";
+import Home from './pages/home';
+import Contact from './pages/contact';
+import About from './pages/about';
+import Dashboard from './pages/dashboard';
 
 function App() {
 
   return (
     <div className='bg-red-700 text-green-500 text-6xl underline '>
-      Hello World
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="courses/:courseId" element={<Dashboard />} />
+      </Routes>
     </div>
   )
 }
