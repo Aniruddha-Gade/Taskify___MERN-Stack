5740dc5925be5b1e924bb3fcf367ab4e3e110dd5
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-29 23:46:50 +0530

    add: auth slice for storing signup data

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import rootReducer from './reducer/index.js'
 
+
+const store = configureStore({
+  reducer: rootReducer
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter >
 )
