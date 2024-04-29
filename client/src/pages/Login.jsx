d45e1a6886dece440a2b21f90372670e3468c352
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-30 00:03:09 +0530

    update: login form subheading

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,8 +5,8 @@ function Login() {
   return (
     <Template
       title="Welcome Back"
-      description1="Build skills for today, tomorrow, and beyond."
-      description2="Education to future-proof your career."
+      description1="Streamline Your Task Management and"
+      description2="Start Organizing Today!"
       image={loginImg}
       formType="login"
     />
