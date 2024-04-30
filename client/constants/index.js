406b5b70ba36102dc898679507bf2ba35e15677c
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-30 20:20:46 +0530

    add: sidebar links data

---
+++ client/constants/index.js
@@ -0,0 +1,30 @@
+
+
+const menu = [
+  {
+    id: 1,
+    title: "All Tasks",
+    icon: '',
+    link: "/dashboard",
+  },
+  {
+    id: 2,
+    title: "Important!",
+    icon: '',
+    link: "/dashboard/important",
+  },
+  {
+    id: 3,
+    title: "Completed!",
+    icon: '',
+    link: "/dashboard/completed",
+  },
+  {
+    id: 4,
+    title: "Do It Now",
+    icon: '',
+    link: "/dashboard/incomplete",
+  },
+];
+
+export default menu;
