501b306343c98bfc7451011d8c4f2fb7e154b65e
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-04-30 23:37:16 +0530

    add: icons for sidebar menu

--- client/constants/index.js
+++ client/constants/index.js
@@ -1,30 +1,33 @@
+import { FaTasks } from "react-icons/fa";
+import { IoHomeSharp } from "react-icons/io5";
+import { IoMdDoneAll } from "react-icons/io";
+import { MdIncompleteCircle } from "react-icons/md";
 
-
-const menu = [
+export const sidebarMenu = [
   {
     id: 1,
     title: "All Tasks",
-    icon: '',
+    icon: IoHomeSharp,
     link: "/dashboard",
   },
   {
     id: 2,
     title: "Important!",
-    icon: '',
+    icon: FaTasks,
     link: "/dashboard/important",
   },
   {
     id: 3,
     title: "Completed!",
-    icon: '',
+    icon: IoMdDoneAll,
     link: "/dashboard/completed",
   },
   {
     id: 4,
     title: "Do It Now",
-    icon: '',
+    icon: MdIncompleteCircle,
     link: "/dashboard/incomplete",
   },
 ];
 
-export default menu;
+
