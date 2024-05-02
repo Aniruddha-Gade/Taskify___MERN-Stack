2cb736d254b5c3112e9019fd78ff617ef320ca63
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-02 21:36:20 +0530

    update: about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>about</div>
+    <div className='flex-center wrapper h-screen w-full mt-[70px]  '>
+      <h2 className='text-7xl '>
+        About
+      </h2>
+    </div>
   )
 }
 
