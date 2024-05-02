dac6a22e065c96407c116f07ea4ff51c79d4bf13
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-02 22:09:21 +0530

    update: todo modal

--- client/src/components/core/modal/TodoModal.jsx
+++ client/src/components/core/modal/TodoModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useForm } from "react-hook-form"
 import { createTask, updateTask } from '../../../services/operations/todoApi';
@@ -10,7 +10,7 @@ import { RxCross2 } from "react-icons/rx";
 const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
 
     const { user: { token } } = useSelector(state => state.profile)
-    const { register, handleSubmit, setValue, getValues, reset, formState: { errors } } = useForm()
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm()
 
 
 
@@ -48,10 +48,9 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
             className={`modal ${showModal ? 'overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full' : 'hidden'}`}
         >
 
-            {/* <div id="create-todo-modal" tabindex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"> */}
             <div className="relative p-4 w-full max-w-md max-h-full">
                 {/* Modal content  */}
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div className="relative bg-blackBg rounded-lg shadow">
                     {/* Modal header  */}
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
@@ -60,11 +59,10 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
                         </h3>
                         {/* close modal */}
                         <button type="button" onClick={() => setShowModal(!showModal)}
-                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg 
-                                            text-lg w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600
-                                           dark:hover:text-white" data-modal-toggle="crud-modal">
+                            className="text-gray-400 bg-transparent hover:bg-[#303030] hover:text-white rounded-lg 
+                                            text-lg w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="crud-modal"
+                        >
                             <RxCross2 />
-
                         </button>
                     </div>
 
@@ -73,7 +71,7 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
                         <div className="grid gap-4 mb-4 grid-cols-2">
                             {/* todo title */}
                             <div className="col-span-2">
-                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                                <label htmlFor="title" className="block mb-2 text-sm font-medium text-white">Title</label>
                                 <input
                                     type="text"
                                     name="title"
@@ -81,9 +79,9 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
                                     {...register("title", { required: true })}
                                     placeholder="Type Todo title"
                                     required
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600
-                                                  focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
-                                                dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    className=" border text-sm rounded-lg focus:ring-primary-600
+                                                  focus:border-primary-600 block w-full p-2.5 bg-[#303030] border-gray-500 placeholder-gray-400
+                                                text-white focus:ring-primary-500 focus:border-primary-500"
                                 />
                                 {errors.title && (
                                     <span className="ml-2 text-xs tracking-wide text-pink-200">
@@ -94,7 +92,7 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
 
                             {/* todo description */}
                             <div className="col-span-2">
-                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-white">
                                     Todo Description
                                 </label>
                                 <textarea
@@ -103,9 +101,8 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
                                     {...register("description", { required: true })}
                                     required
                                     placeholder="Write Todo description here"
-                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300
-                                         focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
-                                          dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="block p-2.5 w-full text-sm rounded-lg border bg-[#303030] border-gray-500 placeholder-gray-400
+                                          text-white focus:ring-blue-500 focus:border-blue-500"
                                 >
                                 </textarea>
                                 {errors.title && (
@@ -124,15 +121,15 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
                                     id="date"
                                     {...register("date", { required: true })}
                                     required
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600
-                                                  focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400
-                                                dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    className=" border text-sm rounded-lg focus:ring-primary-600
+                                                  focus:border-primary-600 block w-full p-2.5 bg-[#303030] border-gray-500 placeholder-gray-400
+                                                text-white focus:ring-primary-500 focus:border-primary-500"
                                 />
                             </div>
 
                             {/* todo completed */}
                             <div className="flex items-center justify-between w-full col-span-2">
-                                <label htmlFor="isCompleted" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Todo Completed</label>
+                                <label htmlFor="isCompleted" className="block mb-2 text-sm font-medium text-white">Todo Completed</label>
                                 <input
                                     type="checkbox"
                                     name="isCompleted"
@@ -145,7 +142,7 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
 
                             {/* todo important */}
                             <div className="flex items-center justify-between w-full col-span-2">
-                                <label htmlFor="isImportant" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Todo Important</label>
+                                <label htmlFor="isImportant" className="block mb-2 text-sm font-medium text-white">Todo Important</label>
                                 <input
                                     type="checkbox"
                                     name="isImportant"
@@ -158,7 +155,7 @@ const TodoModal = ({ showModal, setShowModal, type, selectedTodo }) => {
 
                         </div>
                         <button type="submit"
-                            className="text-white inline-flex gap-2 items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            className="text-white inline-flex gap-2 items-center focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                             {type === 'create' &&
                                 <>
                                     <FaPlus className="text-lg" />
