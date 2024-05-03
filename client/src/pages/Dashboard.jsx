2ddd10896c7cb4385551dbed024dea6607358618
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-03 15:15:13 +0530

    add: open/close button for dashboard sidebar - only for small devices

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
 import Sidebar from './../components/core/dashboard/Sidebar';
 import { Outlet } from 'react-router-dom'
+import { BiMenuAltLeft } from "react-icons/bi";
 
 const Dashboard = () => {
-  return (
-    <div className='h-screen bg-blackBg wrapper flex w-screen'>
-      <Sidebar />
 
-      <div className='w-ful overflow-auto '>
+  const [showSidebar, setShowSidebar] = useState(false)
+
+  return (
+    <div className='h-screen bg-blackBg wrapper flex w-screen relative'>
+      {/* button of open/close sidebar - only for small devices */}
+      <button className='sm:hidden fixed top-[12%] left-1 ' onClick={() => setShowSidebar(!showSidebar)}>
+        <BiMenuAltLeft className='text-2xl text-white' />
+      </button>
+
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+
+      <div className='overflow-auto '>
         <div className='mx-auto w-[96%]  '>
           <Outlet />
         </div>
