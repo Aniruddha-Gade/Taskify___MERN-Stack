034faa5abb6eaf9529c52b61096546d66bcaf672
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-05-06 00:35:16 +0530

    add: footer links

--- client/constants/index.js
+++ client/constants/index.js
@@ -3,6 +3,14 @@ import { IoHomeSharp } from "react-icons/io5";
 import { IoMdDoneAll } from "react-icons/io";
 import { MdIncompleteCircle } from "react-icons/md";
 
+// our Partners images
+import amazonLogo from '../public/images/amazon-logo.png'
+import slackLogo from '../public/images/slack-logo.png'
+import metaLogo from '../public/images/meta-logo.png'
+import swiggyLogo from '../public/images/swiggy-logo.png'
+import dream11Logo from '../public/images/dream11-logo.png'
+
+
 export const sidebarMenu = [
   {
     id: 1,
@@ -31,3 +39,31 @@ export const sidebarMenu = [
 ];
 
 
+
+export const ourPartners = [
+  {
+    id: 1,
+    title:'amazon',
+    image: amazonLogo,
+  },
+  {
+    id: 2,
+    title:'slack',
+    image: slackLogo,
+  },
+  {
+    id: 3,
+    title:'meta',
+    image: metaLogo,
+  },
+  {
+    id: 4,
+    title:'swiggy',
+    image: swiggyLogo,
+  },
+  {
+    id: 5,
+    title:'swiggy',
+    image: dream11Logo,
+  },
+]
